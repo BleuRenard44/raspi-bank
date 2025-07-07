ba6672ab57b4938cc6501510eff1e448ae7b662c
BleuRenard <capitaine.wawa@gmail.com>
2025-07-07 19:32:29 +0200

    update rfid

--- banque_rfid/rfid.py
+++ banque_rfid/rfid.py
@@ -1,16 +1,12 @@
-from MFRC522 import MFRC522
+import gpiozero as GPIO
+from mfrc522 import MFRC522
 
 reader = MFRC522()
 
 def read_uid():
-    print("Scan a card...")
-    (status, tag_type) = reader.MFRC522_Request(reader.PICC_REQIDL)
-    if status != reader.MI_OK:
-        return None
-
-    (status, uid) = reader.MFRC522_Anticoll()
-    if status != reader.MI_OK:
-        return None
-
-    uid_hex = ''.join([hex(x)[2:] for x in uid])
-    return uid_hex
+    try:
+        print("Scan a card...")
+        uid, _ = reader.read()
+        return hex(uid)
+    finally:
+        GPIO.cleanup()
