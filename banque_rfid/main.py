f1191f97ede91d67b50b5c5fce532481f2d05a8c
BleuRenard <capitaine.wawa@gmail.com>
2025-07-07 18:09:12 +0200

    first

--- banque_rfid/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes import accounts, products, payments
-
-app = FastAPI()
-
-app.include_router(accounts.router)
-app.include_router(products.router)
-app.include_router(payments.router)
