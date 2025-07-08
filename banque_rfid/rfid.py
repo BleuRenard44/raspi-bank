7e0f0fd0c0af0c87693bcbed0c2ec10e41d885d6
BleuRenard <capitaine.wawa@gmail.com>
2025-07-08 13:45:33 +0200

    update routes

--- banque_rfid/rfid.py
+++ banque_rfid/rfid.py
@@ -1,6 +1,7 @@
 from rc522_spi_library import RC522SPILibrary
 
-reader = RC522SPILibrary()
+reader = RC522SPILibrary(reset_chip="GPIO25", chip_select=0, bus=0, device=0)
+
 
 def read_uid():
     uid = reader.read_uid()
